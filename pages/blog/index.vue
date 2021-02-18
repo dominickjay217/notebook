f79d94650dee439a12b38861866a67da34aec96b
dominickjay217 <dominickjay217@gmail.com>
2021-02-18 14:15:18 +0000

    Lots of changes to setup blog and about pages

---
+++ pages/blog/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <SocialBar :person="person" />
+    <HeaderBar :person="person" />
+    <BannerBar :person="person" />
+    <main>
+      <div class="container">
+        <section class="content blog">
+            <BlogPosts :posts="posts"/>
+        </section>
+      </div>
+      <BannerBar :person="person"  />
+    </main>
+    <FooterBar />
+    <!-- <code>{{ person.fields }}</code> -->
+  </div>
+</template>
+
+<script>
+import { createClient } from '~/plugins/contentful.js'
+import HeaderBar from '~/components/HeaderBar.vue'
+import BannerBar from '~/components/BannerBar.vue'
+import BlogPosts from '~/components/BlogPosts.vue'
+import NavigationBar from '~/components/NavigationBar.vue'
+import SocialBar from '~/components/SocialBar.vue'
+import FooterBar from '~/components/FooterBar.vue'
+const client = createClient()
+
+export default {
+  components: {
+    HeaderBar,
+    NavigationBar,
+    BannerBar,
+    BlogPosts,
+    SocialBar,
+    FooterBar
+  },
+  asyncData({ env }) {
+    return Promise.all([
+      client.getEntries({
+        content_type: env.CTF_BLOG_POST_TYPE_ID,
+        order: '-sys.createdAt',
+      }),
+      client.getEntries({
+        'sys.id': env.CTF_PERSON_ID,
+      }),
+    ])
+      .then(([posts, entries]) => {
+        return {
+          posts: posts.items,
+          person: entries.items[0],
+        }
+      })
+      .catch(console.error)
+  },
+}
+</script>
