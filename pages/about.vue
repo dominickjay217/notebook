2dd73ef2de66abf4ddcff63da7cceef82a91db14
dominickjay217 <dominickjay217@gmail.com>
2021-03-10 15:49:12 +0000

    Grid change for about

--- pages/about.vue
+++ pages/about.vue
@@ -157,7 +157,7 @@ export default {
 
 .about {
     display: grid;
-    grid-template-columns: repeat(4, minmax(100px, 1fr));
+    grid-template-columns: repeat(auto-fill,minmax(200px,1fr));
     grid-gap: var(--grid-gap);;
     &__intro {
         grid-column: 1 / 4;
