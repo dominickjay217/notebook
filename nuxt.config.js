784b2123dd4ef23a033b3086a97627eda34b38ce
dominickjay217 <dominickjay217@gmail.com>
2021-03-31 15:23:48 +0100

    Change directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/css/remedy.css'],
+  css: ['@/static/css/remedy.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
