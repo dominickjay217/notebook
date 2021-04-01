5dcafbec53afe058f6b7e4e3f6fddd6e12fc1494
dominickjay217 <dominickjay217@gmail.com>
2021-04-01 13:29:50 +0100

    Fluid typography added. Renaming blog to writing.

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@
 
 <style lang="scss">
 
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,700;0,900;1,300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,700;0,900;1,300;1,500;1,700;1,900&display=swap');
 
 @font-face {
   font-family: var(--font-family-headings);
@@ -33,6 +33,82 @@
   --font-family-headings: 'SaltedMocha-Regular';
 }
 
+:root {
+  --fluid-min-width: 326;
+  --fluid-max-width: 1140;
+
+  --fluid-screen: 100vw;
+  --fluid-bp: calc(
+    (var(--fluid-screen) - var(--fluid-min-width) / 16 * 1rem) /
+      (var(--fluid-max-width) - var(--fluid-min-width))
+  );
+}
+
+@media screen and (min-width: 1140px) {
+  :root {
+    --fluid-screen: calc(var(--fluid-max-width) * 1px);
+  }
+}
+
+:root {
+
+  --f--3-min: 10.42;
+  --f--3-max: 12.29;
+  --step--3: calc(
+    ((var(--f--3-min) / 16) * 1rem) + (var(--f--3-max) - var(--f--3-min)) *
+      var(--fluid-bp)
+  );
+
+  --f--2-min: 12.50;
+  --f--2-max: 15.36;
+  --step--2: calc(
+    ((var(--f--2-min) / 16) * 1rem) + (var(--f--2-max) - var(--f--2-min)) *
+      var(--fluid-bp)
+  );
+
+  --f--1-min: 15.00;
+  --f--1-max: 19.20;
+  --step--1: calc(
+    ((var(--f--1-min) / 16) * 1rem) + (var(--f--1-max) - var(--f--1-min)) *
+      var(--fluid-bp)
+  );
+
+  --f-0-min: 18.00;
+  --f-0-max: 24.00;
+  --step-0: calc(
+    ((var(--f-0-min) / 16) * 1rem) + (var(--f-0-max) - var(--f-0-min)) *
+      var(--fluid-bp)
+  );
+
+  --f-1-min: 21.60;
+  --f-1-max: 30.00;
+  --step-1: calc(
+    ((var(--f-1-min) / 16) * 1rem) + (var(--f-1-max) - var(--f-1-min)) *
+      var(--fluid-bp)
+  );
+
+  --f-2-min: 25.92;
+  --f-2-max: 37.50;
+  --step-2: calc(
+    ((var(--f-2-min) / 16) * 1rem) + (var(--f-2-max) - var(--f-2-min)) *
+      var(--fluid-bp)
+  );
+
+  --f-3-min: 31.10;
+  --f-3-max: 46.88;
+  --step-3: calc(
+    ((var(--f-3-min) / 16) * 1rem) + (var(--f-3-max) - var(--f-3-min)) *
+      var(--fluid-bp)
+  );
+
+  --f-4-min: 37.32;
+  --f-4-max: 58.59;
+  --step-4: calc(
+    ((var(--f-4-min) / 16) * 1rem) + (var(--f-4-max) - var(--f-4-min)) *
+      var(--fluid-bp)
+  );
+}
+
 @media screen and (prefers-reduced-motion: reduce) {
   * {
     /* Very short durations means JavaScript that relies on events still works */
@@ -43,17 +119,17 @@
 }
 
 body {
-  font-size: 18px;
+  font: var(--step-0)/1.6;
   font-weight: 300;
   padding: 0;
   margin: 0;
-  line-height: 1.7;
   font-family: var(--font-family-body);
   overflow-x: hidden;
 }
 
 body.template--dark {
   --color-font: var(--color-primary);
+  --background: #222;
 }
 
 *:focus {
@@ -72,11 +148,12 @@ main {
   padding-top: 40px;
   position: relative;
   z-index: 2;
-  background-color: #fff;
+  background-color: var(--color-body);
 }
 
 .template--dark main {
-  background-color: #222;
+  --color-body: #222;
+  background-color: var(--color-body);
 }
 
 .container {
@@ -98,7 +175,7 @@ main {
 }
 
 strong {
-  font-size: 1.25rem;
+  // font-size: 1.25rem;
 }
 
 .spacer {
@@ -130,7 +207,7 @@ strong {
   font-family: var(--font-family-headings);
   &--two {
     text-align: center;
-    font-size: 3rem;
+    font-size: var(--step-3);
   }
 }
 
