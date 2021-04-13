c22483abc5e9491292a15fb20bcc82f4d5e654ba
dominickjay217 <dominickjay217@gmail.com>
2021-04-13 14:26:53 +0100

    Change function

--- netlify/functions/publish-tweet/publish-tweet.js
+++ netlify/functions/publish-tweet/publish-tweet.js
@@ -1,17 +1,53 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+require('dotenv').config();
+
+const Twitter = require('twitter');
+
+const T = new Twitter({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_BEARER_TOKEN,
+  access_token_key: process.env.TWITTER_API_KEY,
+  access_token_secret: process.env.TWITTER_SECRET_KEY
+});
+
+var today = new Date();
+var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+
+function handler ({headers, body}, context, callback) {
+  let promise = new Promise((resolve, reject) => {
+    base('Devtips').select({
+      filterByFormula: `date = "${date}" `,
+    }).eachPage(function page(records, fetchNextPage) {
+      records.forEach(function(record) {
+        resolve(record)
+      });
+    }, function done(error) {
+      console.log(error)
+    });
+  })
+
+  promise.then(function(record) {
+    if(!(record.fields.used == "true")) {
+      let id = record.id;
+      let message = record.fields.message;
+      let article = record.fields.article;
+      let tag = record.fields.tag.split(',').join('');
+      let tweet = `${message} ${article} ${tag}`;
+      T.post('statuses/update', {status: tweet}, function(error, tweet, response) {
+        if (!error) {
+          base('Devtips').update(id, {
+            "used": "true"
+          })
+          callback(null,{
+            statusCode: 200,
+            body: "Success"
+          })
+        }
+      });
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+  })
+  .catch(function(error) {
+    console.log('Error-->', error)
+  })
 }
 
-module.exports = { handler }
+exports.handler = handler;
