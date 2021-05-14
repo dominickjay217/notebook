01010561d51b3f8f7dc51432fafabed06267d32c
dominickjay217 <dominickjay217@gmail.com>
2021-05-14 14:32:07 +0100

    Corrected file

--- netlify/functions/rss/rss.js
+++ netlify/functions/rss/rss.js
@@ -79,8 +79,7 @@ function buildRssItems(items) {
 }
 
 exports.handler = async function (event, context) {
-  const rssFeed = `
-  <?xml version="1.0"?>
+  const rssFeed = `<?xml version="1.0"?>
   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>dominickjay.com</title>
@@ -89,8 +88,7 @@ exports.handler = async function (event, context) {
     <description>dominickjay.com is a lightweight microblogging site powered by Contentful and vanilla HTML, CSS and JavaScript.</description>
     ${buildRssItems(await getPosts())}
   </channel>
-  </rss>
-  `;
+  </rss>`;
 
   return {
     statusCode: 200,
