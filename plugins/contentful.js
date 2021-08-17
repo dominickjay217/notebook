0754af8d738b48df65562b5fc2fc3ebcb876615e
dominickjay217 <dominickjay217@gmail.com>
2021-08-17 08:33:40 +0100

    Remove comment

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -19,9 +19,5 @@ module.exports = {
     }
 
     return contentful.createClient(options)
-    // return contentful.createClient({
-    //   space: config.CTF_SPACE_ID,
-    //   accessToken: config.CTF_CDA_ACCESS_TOKEN
-    // })
   }
 }
